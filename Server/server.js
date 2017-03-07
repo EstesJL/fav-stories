dfff49f431b65c1fb17f522083b7c89636669dd9
Jordan Estes <jle48@georgetown.edu>
2017-03-07 16:22:52 -0500

    REDIRECT FIX

--- Server/server.js
+++ Server/server.js
@@ -75,7 +75,7 @@ app.post('/posts', function(req, res, next) {
 
 app.get('/', function(req, res) {
   console.log('PATH JOIN', path.join(__dirname, '../client/index.html'));
-  res.sendfile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 
